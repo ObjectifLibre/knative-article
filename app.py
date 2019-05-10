55dc516788130e04fe1b25072f8dc100eb6d594a
Jacques Roussel <jacques.roussel@objectif-libre.com>
2019-05-10 16:11:45 +0200

    Init

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    return 'Hello world!\n'
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
